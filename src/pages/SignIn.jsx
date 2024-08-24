4fb7d30f745944881f90d874fbc6511ea898f1ab
mohit0317 <mohitkhodse44@gmail.com>
2024-08-24 16:02:18 +0530

    Sign up page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.css';
 
 const SignIn = () => {
+
+    const [active, setActive] = useState('signin');
+
     return (
         <div className='signin-page'>
             <div className="">
-                <h1>Sign In</h1>
+                <h1>{active === 'signin' ? 'Sign In' : 'Sign Up'}</h1>
+                {active === 'signup' &&
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Username"
+                    />
+                }
                 <input
                     type="text"
                     className="form-control"
@@ -16,9 +26,13 @@ const SignIn = () => {
                     className="form-control"
                     placeholder="Password"
                 />
-                <button type='button' className='btn btn-secondary'>SIGN IN</button>
+                <button type='button' className='btn btn-secondary'>{active === 'signin' ? 'SIGN IN' : 'SIGN UP'}</button>
                 <button type='button' className='btn btn-danger'>CONTINUE WITH GOOGLE </button>
-                <p>Dont have an account? <span> Sign up</span></p>
+                {active === 'signin'
+                    ?
+                    <p>Dont have an account? <span onClick={() => setActive('signup')} > Sign up</span></p>
+                    :
+                    <p>Have an account? <span onClick={() => setActive('signin')} > Sign in</span></p>}
             </div>
         </div>
     )
